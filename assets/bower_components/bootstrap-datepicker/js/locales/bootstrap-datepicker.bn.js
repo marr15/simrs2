9438da3ef965d863ca106c94e8a08421afae0d9e
Sulisnawatii <40849001+Sulisnawatii@users.noreply.github.com>
2018-07-15 12:26:47 +0800

    Update bootstrap-datepicker.bn.js

--- assets/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.bn.js
+++ assets/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.bn.js
@@ -1,7 +1,7 @@
 /**
  * Bengali (Bangla) translation for bootstrap-datepicker
  * Karim Khan <kkhancse91@gmail.com>
- * Orif N. Jr. <orif.zade@gmail.com>
+ * Orif N. Jr. <orif.zade@gmail.com> 
  */
 ;(function($){
   $.fn.datepicker.dates['bn'] = {
