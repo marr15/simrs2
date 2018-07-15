08234ee8d7d040bbe65da944428f3083c0320641
Sulisnawatii <40849001+Sulisnawatii@users.noreply.github.com>
2018-07-15 12:28:08 +0800

    Update bootstrap-datepicker.zh-TW.js

--- assets/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.zh-TW.js
+++ assets/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.zh-TW.js
@@ -1,6 +1,6 @@
 /**
  * Traditional Chinese translation for bootstrap-datepicker
- * Rung-Sheng Jang <daniel@i-trend.co.cc>
+ * Rung-Sheng Jang <daniel@i-trend.co.cc> 
  * FrankWu  <frankwu100@gmail.com> Fix more appropriate use of Traditional Chinese habit
  */
 ;(function($){
