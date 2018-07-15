f268d589a3025cfe75aa4fa8640aa79175bc9fb8
Sulisnawatii <40849001+Sulisnawatii@users.noreply.github.com>
2018-07-15 12:34:40 +0800

    Update bootstrap-datepicker.rs-latin.js

--- assets/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.rs-latin.js
+++ assets/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.rs-latin.js
@@ -1,5 +1,5 @@
 /**
- * Serbian latin translation for bootstrap-datepicker
+ * Serbian latin translation for bootstrap-datepicker 
  * Bojan Milosavlević <milboj@gmail.com>
  *
  * DEPRECATED: This language code 'rs-latin' is deprecated (invalid serbian language code) and will be removed in 2.0.
